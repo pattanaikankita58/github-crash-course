1d29ec8238bb293fb75dc34f966c91eaed4275e7
pattanaik.ankita <pattanaik.ankita51@gmail.com>
2025-10-15 19:46:42 +0530

    initial code added

---
+++ Example.java
@@ -0,0 +1,10 @@
+public class Example {
+    public static void main(String[] args)
+     {
+         int a=10;
+         String name="Ankita";
+            System.out.println("Hello, World! " + name + " " + a);
+    }
+
+
+}
